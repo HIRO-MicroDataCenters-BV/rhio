377804d31888e4892e392dcca10085cb583f663a
Sam Andreae <contact@samandreae.com>
2024-07-25 07:49:54 +0100

    Add error type

--- rhio-py/src/lib.rs
+++ rhio-py/src/lib.rs
@@ -5,8 +5,28 @@ use uniffi;
 
 uniffi::setup_scaffolding!();
 
+#[derive(Debug, thiserror::Error, uniffi::Object)]
+#[error("{e:?}")]
+#[uniffi::export(Debug)]
+pub struct RhioError {
+    e: anyhow::Error,
+}
+
+#[uniffi::export]
+impl RhioError {
+    pub fn message(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl From<anyhow::Error> for RhioError {
+    fn from(e: anyhow::Error) -> Self {
+        Self { e }
+    }
+}
+
 #[derive(uniffi::Object)]
-struct Node {
+pub struct Node {
     pub inner: RhioNode,
 }
 
@@ -19,11 +39,10 @@ impl Node {
 }
 
 #[uniffi::export(async_runtime = "tokio")]
-pub async fn run() -> Node {
+pub async fn run() -> Result<Node, RhioError> {
     let config = Config::default();
     let private_key = generate_ephemeral_private_key();
     let rhio_node = RhioNode::spawn(config.network_config, private_key)
-        .await
-        .expect("Node can start");
-    Node { inner: rhio_node }
+        .await?;
+    Ok(Node { inner: rhio_node })
 }
