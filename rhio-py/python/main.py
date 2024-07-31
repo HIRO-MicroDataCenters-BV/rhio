20f117febacf9dcc0babad9ddf9d8f7a44c8ff0b
Sam Andreae <contact@samandreae.com>
2024-07-31 14:53:44 +0100

    WIP: subscribe to gossip topics from Python

--- rhio-py/python/main.py
+++ rhio-py/python/main.py
@@ -1,15 +1,36 @@
-import rhio
-
 import asyncio
+
+from rhio import rhio_ffi, Node, GossipMessageCallback, Config, Message
+
+class Callback(GossipMessageCallback):
+    def __init__(self, name):
+        self.name = name
+        self.chan = asyncio.Queue()
+
+    async def on_message(self, msg):
+        print("received message ", msg.content(), " from ", msg.delivered_from())
+        await self.chan.put(msg)
 
 async def main():
     # setup event loop, to ensure async callbacks work
-    rhio.rhio_ffi.uniffi_set_event_loop(asyncio.get_running_loop())
+    loop = asyncio.get_running_loop()
+    rhio_ffi.uniffi_set_event_loop(loop)
 
-    config = rhio.Config()
+    config = Config()
+    node = await Node.spawn(config)
+    print("Node ID: ", node.id())
 
-    node = await rhio.Node.spawn(config)
-    print(node.id())
+    topic = "test"
+    cb = Callback("n0")
+
+    print("subscribing to gossip topic")
+    sender = await node.subscribe(topic, cb)
+
+    print("gossip topic ready")
+
+    while True:
+        await asyncio.sleep(1)
+        await sender.send(Message(bytearray("hello".encode("utf-8"))))
 
 if __name__ == "__main__":
     asyncio.run(main())
