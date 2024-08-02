626f2bd7b06e04f7495e16fd8130b10fc889e031
Sam Andreae <contact@samandreae.com>
2024-08-02 08:12:21 +0100

    Re-organize code a little

---
+++ rhio-py/src/messages.rs
@@ -0,0 +1,144 @@
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use rhio::messages::{Message as InnerMessage, MessageMeta as InnerMessageMeta};
+use rhio::node::TopicSender;
+
+use crate::error::{CallbackError, RhioError};
+
+type Hash = String;
+type Path = String;
+
+#[derive(Clone, Debug, uniffi::Object)]
+pub enum FileSystemEvent {
+    /// Announce that a file was created on the local file-system
+    Create(Path, Hash),
+    // ... more as yet unsupported events
+}
+
+#[derive(Clone, Debug, uniffi::Object)]
+pub enum Message {
+    // Sync files in a directory
+    FileSystem(FileSystemEvent),
+
+    // Share arbitrary blobs
+    BlobAnnouncement(Hash),
+
+    // Application messages
+    Application(Vec<u8>),
+}
+
+#[derive(Debug, uniffi::Enum)]
+pub enum MessageType {
+    FileSystem,
+    BlobAnnouncement,
+    Application,
+}
+
+#[uniffi::export]
+impl Message {
+    pub fn r#type(&self) -> MessageType {
+        match self {
+            Message::FileSystem(_) => MessageType::FileSystem,
+            Message::BlobAnnouncement(_) => MessageType::BlobAnnouncement,
+            Message::Application(_) => MessageType::Application,
+        }
+    }
+
+    pub fn as_file_system(&self) -> FileSystemEvent {
+        if let Self::FileSystem(fs_event) = self {
+            fs_event.clone()
+        } else {
+            panic!("not a FileSystem message");
+        }
+    }
+
+    pub fn as_blob_announcement(&self) -> String {
+        if let Self::BlobAnnouncement(s) = self {
+            s.clone()
+        } else {
+            panic!("not a NeighborDown message");
+        }
+    }
+
+    pub fn as_application(&self) -> Vec<u8> {
+        if let Self::Application(s) = self {
+            s.clone()
+        } else {
+            panic!("not a Application message");
+        }
+    }
+}
+
+#[derive(Debug, Clone, uniffi::Object)]
+pub struct MessageMeta(pub(crate) InnerMessageMeta);
+
+#[uniffi::export]
+impl MessageMeta {
+    #[uniffi::method]
+    pub fn delivered_from(&self) -> String {
+        self.0.delivered_from.to_string()
+    }
+
+    pub fn operation_timestamp(&self) -> u64 {
+        self.0.operation_timestamp
+    }
+}
+
+impl From<rhio::messages::Message<Vec<u8>>> for Message {
+    fn from(value: rhio::messages::Message<Vec<u8>>) -> Self {
+        match value {
+            InnerMessage::FileSystem(rhio::messages::FileSystemEvent::Create(path, hash)) => {
+                Message::FileSystem(FileSystemEvent::Create(
+                    path.to_string_lossy().to_string(),
+                    hash.to_string(),
+                ))
+            }
+            InnerMessage::BlobAnnouncement(hash) => Message::BlobAnnouncement(hash.to_string()),
+            InnerMessage::Application(bytes) => Message::Application(bytes),
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl TryFrom<Message> for rhio::messages::Message<Vec<u8>> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Message) -> Result<Self, Self::Error> {
+        let value = match value {
+            Message::FileSystem(FileSystemEvent::Create(path, hash)) => {
+                rhio::messages::Message::FileSystem(rhio::messages::FileSystemEvent::Create(
+                    PathBuf::from(path),
+                    hash.parse()?,
+                ))
+            }
+            Message::BlobAnnouncement(hash) => {
+                rhio::messages::Message::BlobAnnouncement(hash.parse()?)
+            }
+            Message::Application(bytes) => rhio::messages::Message::Application(bytes),
+        };
+        Ok(value)
+    }
+}
+
+#[uniffi::export(with_foreign)]
+#[async_trait::async_trait]
+pub trait GossipMessageCallback: Send + Sync + 'static {
+    async fn on_message(
+        &self,
+        msg: Arc<Message>,
+        meta: Arc<MessageMeta>,
+    ) -> Result<(), CallbackError>;
+}
+
+#[derive(uniffi::Object)]
+pub struct Sender(pub(crate) TopicSender<Vec<u8>>);
+
+#[uniffi::export]
+impl Sender {
+    pub async fn send(&self, message: &Message) -> Result<MessageMeta, RhioError> {
+        let message = rhio::messages::Message::<Vec<u8>>::try_from(message.clone())?;
+        let meta = self.0.send(message).await?;
+        Ok(MessageMeta(meta))
+    }
+}
--- rhio-py/src/node.rs
+++ rhio-py/src/node.rs
@@ -1,11 +1,15 @@
 use std::path::PathBuf;
+use std::sync::Arc;
 
 use rhio::config::{parse_node_addr, parse_url, Config as RhioConfig, NodeAddr};
 use rhio::private_key::generate_ephemeral_private_key;
+use rhio::topic_id::TopicId;
 use rhio::Node as RhioNode;
+use tracing::warn;
 use uniffi;
 
 use crate::error::RhioError;
+use crate::messages::{GossipMessageCallback, MessageMeta, Sender};
 
 #[derive(Default, Clone, uniffi::Record)]
 pub struct Config {
@@ -70,4 +74,109 @@ impl Node {
     pub fn id(&self) -> String {
         self.inner.id().to_hex()
     }
+
+    #[uniffi::method(async_runtime = "tokio")]
+    pub async fn subscribe(
+        &self,
+        topic: &str,
+        cb: Arc<dyn GossipMessageCallback>,
+    ) -> Result<Sender, RhioError> {
+        let topic_id = TopicId::new_from_str(topic);
+
+        let (topic_tx, mut topic_rx, ready) = self.inner.subscribe(topic_id).await?;
+
+        ready.await;
+
+        tokio::task::spawn(async move {
+            while let Ok((message, meta)) = topic_rx.recv().await {
+                if let Err(err) = cb
+                    .on_message(Arc::new(message.into()), Arc::new(MessageMeta(meta)))
+                    .await
+                {
+                    warn!("cb error, gossip: {:?}", err);
+                }
+            }
+        });
+
+        Ok(Sender(topic_tx))
+    }
 }
+
+
+// @TODO: need to form a connection between the test nodes
+//
+// #[cfg(test)]
+// mod tests {
+//     use std::time::Duration;
+//
+//     use tokio::sync::mpsc;
+//
+//     use crate::node::Config;
+//
+//     use super::*;
+//
+//     #[tokio::test]
+//     async fn test_gossip_basic() {
+//         let config = Config::default();
+//         let n0 = Node::spawn(&config).await.unwrap();
+//         let n1 = Node::spawn(&config).await.unwrap();
+//
+//         let n0_id = n0.id();
+//
+//         struct Cb {
+//             channel: mpsc::Sender<Arc<MessageWithMeta>>,
+//         }
+//         #[async_trait::async_trait]
+//         impl GossipMessageCallback for Cb {
+//             async fn on_message(&self, message: Arc<MessageWithMeta>) -> Result<(), CallbackError> {
+//                 println!("<< {:?}", message);
+//                 self.channel.send(message).await.unwrap();
+//                 Ok(())
+//             }
+//         }
+//
+//         let topic = "test";
+//
+//         let (sender0, _receiver0) = mpsc::channel(8);
+//         let cb0 = Cb { channel: sender0 };
+//
+//         let (sender1, mut receiver1) = mpsc::channel(8);
+//         let cb1 = Cb { channel: sender1 };
+//
+//         let sender0 = n0.subscribe(topic, Arc::new(cb0)).await.unwrap();
+//         let _sender1 = n1.subscribe(topic, Arc::new(cb1)).await.unwrap();
+//
+//         // @TODO: connect nodes
+//
+//         // Send message on n0
+//         println!("sending message");
+//         let msg_content = b"hello";
+//         sender0
+//             .send(&Message(InnerMessage::Application(msg_content.to_vec())))
+//             .await
+//             .unwrap();
+//
+//         // Receive on n1
+//         let recv_fut = async {
+//             loop {
+//                 let Some(event) = receiver1.recv().await else {
+//                     panic!("receiver stream closed before receiving gossip message");
+//                 };
+//                 println!("event: {:?}", event);
+//                 let MessageWithMeta {
+//                     ref content,
+//                     ref meta,
+//                 } = &*event;
+//
+//                 if let InnerMessage::Application(bytes) = content {
+//                     assert_eq!(bytes, msg_content);
+//                 };
+//
+//                 assert_eq!(meta.delivered_from.to_string(), n0_id.to_string());
+//             }
+//         };
+//         tokio::time::timeout(std::time::Duration::from_secs(10), recv_fut)
+//             .await
+//             .expect("timeout reached and no gossip message received");
+//     }
+// }
