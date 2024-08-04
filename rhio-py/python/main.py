d882200b076c3dd1dadaadca27bd12dae5a94b43
Sam Andreae <contact@samandreae.com>
2024-08-04 15:09:33 +0100

    FileSystemSync is a sub-class of GossipMessageCallback

--- rhio-py/python/main.py
+++ rhio-py/python/main.py
@@ -8,24 +8,20 @@ from watchfiles import awatch, Change
 
 EXPORTED_BLOBS = {}
 
-class Callback(GossipMessageCallback):
-    def __init__(self, name):
-        self.name = name
-        self.chan = asyncio.Queue()
-
-    async def on_message(self, msg, meta):
-        await self.chan.put((msg, meta))
-
 class Watcher():
+    """A watcher which monitors a directory on the file-system and broadcasts `FileSystem` gossip
+    events whenever it is notified that a file has been added"""
     def __init__(self, sender, node, blobs_dir_path):
         self.sender = sender
         self.node = node
         self.blobs_dir_path = blobs_dir_path
 
     def relative_path(self, path):
+        """Get the relative path to a file added to the base directory"""
         return os.path.relpath(path, self.blobs_dir_path)
 
     async def handle_change(self, change_type, path):
+        """handle a change event"""
         # we only handle "added" events
         if change_type == 1:
             # calculate path of blob relative to blobs dir
@@ -47,21 +43,34 @@ class Watcher():
             await self.sender.send(msg)
 
     async def watch(self):
+        """Run the watcher"""
         async for changes in awatch(self.blobs_dir_path):
             for (change_type, path) in changes:
                 await self.handle_change(change_type, path)
 
-class FileSystemSync():
-    def __init__(self, cb, node, blobs_dir_path):
-        self.cb = cb
+class FileSystemSync(GossipMessageCallback):
+    """Aggregator class which uses last-write-wins logic to maintain a deterministic mapping of
+    paths to hashes. Consumes FileSystem events and uses the Node api to download blobs from the
+    network and export them to the file-system"""
+    def __init__(self, node, blobs_dir_path):
         self.node = node
         self.blobs_dir_path = blobs_dir_path
+        self.file_system = {}
 
-    async def handle_event(self, message, meta):
+    async def on_message(self, message, meta):
+        """Process FileSystem events"""
         if (message.type() == MessageType.FILE_SYSTEM):
             create_event = message.as_file_system_create()
             rel_path = create_event.path
             hash = create_event.hash
+
+            # check if there is already a file at this path, if there is, compare it's timestamp
+            # against the incoming events timestamp and only proceed if it is more recent (we
+            # fall-back to comparing hashes if the timestamp is equal)
+            timestamp_and_hash = self.file_system.get(rel_path)
+            if timestamp_and_hash is not None and (meta.operation_timestamp(), hash) < timestamp_and_hash:
+                logger.info("ignoring file addition at existing path which contains a lower timestamp: {} {}", meta.operation_timestamp(), hash)
+                return
 
             # download the blob from the network
             await self.node.download_blob(hash)
@@ -74,15 +83,13 @@ class FileSystemSync():
             await self.node.export_blob(hash, path)
             logger.info("exported blob to file-system: {}", path)
 
+            # add the file to our file-system aggregate
+            self.file_system[rel_path] = (meta.operation_timestamp(), hash)
+
             # record that we just exported a blob to this path
             EXPORTED_BLOBS[rel_path] = hash
         else:
             print("received unsupported event type: {}", event.type())
-
-    async def run(self):
-        while (True):
-            (message, meta) = await self.cb.chan.get()
-            await self.handle_event(message, meta)
 
 async def main():
     # setup event loop, to ensure async callbacks work
@@ -114,25 +121,14 @@ async def main():
     # subscribe to a topic, providing a callback method which will be run on each 
     # topic event we receive
     topic = TopicId.new_from_str("rhio/file_system_sync")
-    cb = Callback("file_system_sync_handler")
+    cb = FileSystemSync(node, config.blobs_path)
 
     logger.info("subscribing to gossip topic: {}", topic)
     sender = await node.subscribe(topic, cb)
     await sender.ready()
 
     logger.info("gossip topic ready")
-
-    if config.blobs_path:
-        watch_task = asyncio.create_task(
-            Watcher(sender, node, config.blobs_path).watch()
-        )
-
-        sync_task = asyncio.create_task(
-            FileSystemSync(cb, node, config.blobs_path).run()
-        )
-
-        await watch_task
-        await sync_task
+    await Watcher(sender, node, config.blobs_path).watch()
 
 if __name__ == "__main__":
     asyncio.run(main())
