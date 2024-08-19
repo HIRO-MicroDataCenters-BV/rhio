455c37bf62407853bbb56c9a5fee5cdfa8b27985
Sam Andreae <contact@samandreae.com>
2024-08-19 13:28:03 +0100

    Print ticket from hello example

--- rhio/examples/hello.rs
+++ rhio/examples/hello.rs
@@ -6,6 +6,7 @@ use p2panda_net::ToBytes;
 use rhio::config::Config;
 use rhio::messages::{Message, MessageMeta};
 use rhio::node::Node;
+use rhio::ticket::Ticket;
 use rhio::topic_id::TopicId;
 use tokio_util::task::LocalPoolHandle;
 
@@ -19,12 +20,27 @@ async fn main() -> Result<()> {
     let chat_topic_id = TopicId::new_from_str("rhio/hello_world");
     let private_key = PrivateKey::new();
     let config = Config::default();
+    let relay = config.network_config.relay.clone();
 
     // Spawn the node
     let node: Node<ChatMessage> =
         Node::spawn(config.clone(), private_key.clone(), pool_handle).await?;
 
-    println!("Peer Id: {}", private_key.public_key().to_hex());
+    if let Some(addresses) = node.direct_addresses().await {
+        match &relay {
+            Some(url) => {
+                println!("‣ relay url: {}", url);
+            }
+            None => {
+                println!("! you haven't specified a relay address for your node");
+                println!("other peers might not be able to connect to you without it.");
+            }
+        }
+        let ticket = Ticket::new(node.id(), addresses, relay);
+        println!("‣ connection ticket: {}", ticket);
+    } else {
+        println!("‣ node public key: {}", node.id());
+    }
 
     println!("joining gossip overlay ..");
     let (chat_tx, mut chat_rx, ready) = node.subscribe(chat_topic_id).await?;
