f2b852a16b4250f32c36d851982bf21003a6a564
Sam Andreae <contact@samandreae.com>
2024-11-04 04:44:36 +0000

    Refactor S3File into methods

--- rhio/src/blobs/store/s3_file.rs
+++ rhio/src/blobs/store/s3_file.rs
@@ -44,15 +44,14 @@ impl S3File {
             self.last_chunk = Some(bytes.to_vec());
             return Ok(());
         }
+
         let chunk: [u8; IROH_BLOCK_SIZE.bytes()] = bytes.try_into()?;
 
         // The part number for this group of chunks.
-        let part_number = offset % IROH_BLOCK_SIZE.bytes() as u64;
+        let part_number = offset_to_part_number(offset);
 
-        self.parts
-            .entry(part_number)
-            .and_modify(|entry| entry.push((offset, chunk)))
-            .or_insert(vec![(offset, chunk)]);
+        // Insert the chunk into the correct part.
+        insert_to_part(&mut self.parts, part_number, (offset, chunk));
 
         // @TODO: as parts meet the minimum upload size then we should already upload them to
         // the minio store.
@@ -92,39 +91,12 @@ impl S3File {
 
         sorted_parts.sort();
         let len = sorted_parts.len();
+
         for (index, (part_number, chunks)) in sorted_parts.iter_mut().enumerate() {
             let mut bytes: Vec<u8> = Vec::new();
-            chunks.sort();
-            for (_, chunk) in chunks {
-                bytes.extend(&chunk[..]);
-            }
-
-            if index == len - 1 {
-                if let Some(last_chunk) = &self.last_chunk {
-                    bytes.extend(last_chunk);
-                }
-            }
-
-            let upload_id = match &self.upload_id {
-                Some(id) => Ok::<_, anyhow::Error>(id.to_owned()),
-                None => {
-                    let mpu = self.bucket.initiate_multipart_upload_blocking(
-                        &self.path,
-                        "application/octet-stream",
-                    )?;
-                    self.upload_id = Some(mpu.upload_id.clone());
-                    Ok(mpu.upload_id.to_owned())
-                }
-            }?;
-
-            let part = self.bucket.put_multipart_chunk_blocking(
-                bytes,
-                &self.path,
-                *part_number as u32,
-                &upload_id,
-                "application/octet-stream",
-            )?;
-
+            concat_chunks(index, len, &mut bytes, chunks, &self.last_chunk);
+            let (part, upload_id) = upload_to_s3(&self.bucket, &self.path, bytes, *part_number, self.upload_id.as_ref())?;
+            self.upload_id = Some(upload_id);
             self.uploaded_parts.push(part);
         }
 
@@ -137,4 +109,64 @@ impl S3File {
             .presign_get_blocking(&self.path, 60 * 60 * 24, None)?;
         Ok(HttpAdapter::new(url::Url::from_str(&url)?))
     }
+}
+
+fn offset_to_part_number(offset: u64) -> u64 {
+    offset % IROH_BLOCK_SIZE.bytes() as u64
+}
+
+fn insert_to_part(
+    parts: &mut HashMap<u64, Vec<(u64, [u8; IROH_BLOCK_SIZE.bytes()])>>,
+    part_number: u64,
+    item: (u64, [u8; IROH_BLOCK_SIZE.bytes()]),
+) {
+    parts
+        .entry(part_number)
+        .and_modify(|entry| entry.push(item))
+        .or_insert(vec![item]);
+}
+
+fn concat_chunks(
+    index: usize,
+    len: usize,
+    bytes: &mut Vec<u8>,
+    chunks: &mut Vec<(u64, [u8; IROH_BLOCK_SIZE.bytes()])>,
+    last_chunk: &Option<Vec<u8>>,
+) {
+    chunks.sort();
+    for (_, chunk) in chunks {
+        bytes.extend(&chunk[..]);
+    }
+
+    if index == len - 1 {
+        if let Some(last_chunk) = last_chunk {
+            bytes.extend(last_chunk);
+        }
+    }
+}
+
+fn upload_to_s3(
+    bucket: &Bucket,
+    path: &str,
+    bytes: Vec<u8>,
+    part_number: u64,
+    upload_id: Option<&String>,
+) -> Result<(Part, String)> {
+    let upload_id = match upload_id {
+        Some(id) => Ok::<_, anyhow::Error>(id.to_owned()),
+        None => {
+            let mpu =
+                bucket.initiate_multipart_upload_blocking(&path, "application/octet-stream")?;
+            Ok(mpu.upload_id.to_owned())
+        }
+    }?;
+
+    let part = bucket.put_multipart_chunk_blocking(
+        bytes,
+        &path,
+        part_number as u32,
+        &upload_id,
+        "application/octet-stream",
+    )?;
+    Ok((part, upload_id))
 }
