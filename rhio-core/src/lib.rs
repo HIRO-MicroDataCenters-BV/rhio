35e488c90723be49e90fa56daf8e958960fb8700
adz <x12@adz.garden>
2024-11-11 15:30:21 +0100

    Remove all unused parts in rhio-core

--- rhio-core/src/lib.rs
+++ rhio-core/src/lib.rs
@@ -1,18 +1,7 @@
 pub mod bucket;
-pub mod extensions;
-pub mod log_id;
-pub mod operation;
 pub mod private_key;
 pub mod subject;
-pub mod topic_id;
 
 pub use bucket::{Bucket, ScopedBucket};
-#[deprecated]
-pub use extensions::{DeprecatedSubject, RhioExtensions};
-pub use log_id::LogId;
-pub use operation::{
-    create_blob_announcement, create_message, create_operation, decode_operation, encode_operation,
-};
 pub use private_key::load_private_key_from_file;
 pub use subject::{ScopedSubject, Subject};
-pub use topic_id::TopicId;
