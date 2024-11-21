e64605cb779e5896bf2a794b917527dc05125700
Sam Andreae <contact@samandreae.com>
2024-11-21 12:43:37 +0000

    WIP: helm charts and build action

--- version.sh
+++ version.sh
@@ -4,7 +4,6 @@ set -o errexit
 set -o nounset
 
 ROOT="${GITHUB_WORKSPACE}"
-CHART_NAME="server"
 
 VERSION_APP_PATH="./VERSION_APP"
 VERSION_CHART_PATH="./VERSION_CHART"
@@ -84,7 +83,7 @@ make_docker_images_with_tags() {
 patch_helm_chart() {
   DOCKER_IMAGE_NAME="$1"
 
-  CHART_PATH="./server/charts/${CHART_NAME}"
+  CHART_PATH="./charts"
 
   DOCKER_IMAGE_TAG=$(rev "${VERSION_DOCKER_PATH}" | cut -d ',' -f 1 | rev)
   VERSION_CHART=$(cat "${VERSION_CHART_PATH}")
